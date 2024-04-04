ee58fdc11c83f34c9df8475482178deb255471e6
YashSharma412 <sharmayashsatiish@gmail.com>
2024-04-05 01:02:02 +0530

    added proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000"
+      "/api": "https://employee-details-mern.onrender.com",
+      // "/api": "http://localhost:3000",
     }
   },
   plugins: [react()],
