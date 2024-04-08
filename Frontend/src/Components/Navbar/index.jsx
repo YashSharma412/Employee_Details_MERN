0cd826ca0a53f557f4bfdfba55c73e7b6de31e75
YashSharma412 <sharmayashsatiish@gmail.com>
2024-04-08 11:49:09 +0530

    implemented logout

--- Frontend/src/Components/Navbar/index.jsx
+++ Frontend/src/Components/Navbar/index.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
 import "./styles.css";
 import { TfiMenu } from "react-icons/tfi";
 import BASE_URL from "../../URL/BASE_URL";
 
 const Navbar = () => {
+  const navigate  = useNavigate()
   const [user, setUser] = useState(()=>getUser());
   // useEffect(()=>{
   //   getUser()
@@ -16,6 +19,19 @@ const Navbar = () => {
       setUser(response.data.data)
     } catch(err){
       console.log(err)
+    }
+  }
+
+  async function handeLogout() {
+    try{
+      const response = await axios.get(`${BASE_URL}/api/auth/logout`, { withCredentials: true})
+      console.log(response.data)
+      if(response.status === 200) {
+        alert("user logged out")
+        navigate("/");
+      }
+    } catch (err){
+      console.log(err.response.data)
     }
   }
 
@@ -31,7 +47,9 @@ const Navbar = () => {
           {"( "}{user.role}{" )"}, {user.name}
         </div>
       }
-      <div></div>
+      <button onClick={handeLogout} className="logout_btn">
+        Logout
+      </button>
     </div>
   );
 };
